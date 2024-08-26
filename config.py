a5f89f14794c76a9a69a98fa087c20475227f664
th.kim <th.kim@hyperinfo.co.kr>
2024-08-26 11:19:06 +0900

    add captcha images

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+import torch
+
 DATA_DIR = "input/captcha_images_v2/"
 BATCH_SIZE = 8
 IMAGE_WIDTH = 300
 IMAGE_HEIGHT = 75
 NUM_WORKERS = 8
 EPOCHS = 200
-DEVICE = "cuda"
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
